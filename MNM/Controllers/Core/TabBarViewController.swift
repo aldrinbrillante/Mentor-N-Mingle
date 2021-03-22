efe3ab0eda457f0378a613965182a9f2047fbef0
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-22 09:50:19 -0700

    tabbarviewcontroller

--- MNM/Controllers/Core/TabBarViewController.swift
+++ MNM/Controllers/Core/TabBarViewController.swift
@@ -11,19 +11,17 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpControllers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpControllers() {
+        let home = HomeViewController()
+        let explore = ExploreViewController()
+        let camera = CameraViewController()
+        let notifications = NotificationsViewController()
+        let profile = ProfileViewController()
     }
-    */
+
+
 
 }
