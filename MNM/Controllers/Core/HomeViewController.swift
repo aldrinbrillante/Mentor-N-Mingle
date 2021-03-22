6c6780e0d20584c101bd30cb15daf01dfbb7b7da
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-22 07:53:29 -0700

    Create HomeViewController.swift

---
+++ MNM/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MNM
+//
+//  Created by Aldrin Brillante on 3/14/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red //initial background color
+    }
+
+
+}
+
