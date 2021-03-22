3b4b60b7e0b25809ad58bb71ea1b82e99c4fd2c6
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-22 13:25:26 -0700

    Mentor assistance in code
    Industry mentor guided me with the understanding of further code, as well as assisted in code structure

--- MNM/Managers/HapticsManager.swift
+++ MNM/Managers/HapticsManager.swift
@@ -5,15 +5,21 @@
 //  Created by Aldrin Brillante on 3/22/21.
 //
 
+
 import Foundation
 import UIKit
 
+/// Object that deals with haptic feedback
 final class HapticsManager {
+    /// Shared singleton instance
     static let shared = HapticsManager()
-    
+
+    /// Private constructor
     private init() {}
-    
-    //Public
+
+    // Public
+
+    /// Vibrate for light selection of item
     public func vibrateForSelection() {
         DispatchQueue.main.async {
             let generator = UISelectionFeedbackGenerator()
@@ -21,6 +27,9 @@ final class HapticsManager {
             generator.selectionChanged()
         }
     }
+
+    /// Trigger feedback vibration based on event type
+    /// - Parameter type: Success, Error, or Warning type
     public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
         DispatchQueue.main.async {
             let generator = UINotificationFeedbackGenerator()
@@ -29,3 +38,4 @@ final class HapticsManager {
         }
     }
 }
+
