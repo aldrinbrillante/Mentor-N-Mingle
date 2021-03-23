7cd399bdb64044624d523c10b7decf679627bec5
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-23 14:32:09 -0700

    final push

---
+++ MNM/Views/Other/ProfileHeaderCollectionReusableView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileHeaderCollectionReusableView.swift
+//  MNM
+//
+//  Created by Aldrin Brillante on 3/22/21.
+//
+
+import Foundation
---
+++ MNM/Views/Other/RecordButton.swift
@@ -0,0 +1,8 @@
+//
+//  RecordButton.swift
+//  MNM
+//
+//  Created by Aldrin Brillante on 3/22/21.
+//
+
+import Foundation
